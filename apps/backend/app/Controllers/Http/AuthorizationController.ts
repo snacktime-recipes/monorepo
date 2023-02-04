b01fe70ee52ebfdeceb08b190e08ad5a1d538b61
NE-student <102172264+NE-student@users.noreply.github.com>
2023-02-04 13:44:30 +0200

    (back) auth-features-step1

--- apps/backend/app/Controllers/Http/AuthorizationController.ts
+++ apps/backend/app/Controllers/Http/AuthorizationController.ts
@@ -1,28 +1,46 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-import Product from 'App/Models/Product';
 import Profile from 'App/Models/Profile';
 import ProfileProduct from 'App/Models/ProfileProduct';
+import ErrorType from 'Types/ErrorType.enum';
 
 export default class AuthorizationController {
     public async login({ auth, request, response }: HttpContextContract) {
         const email = request.input('email');
         const password = request.input('password');
         try{
-        await auth.use('web').attempt(email, password);
+            await auth.use('web').attempt(email, password);
         }
         catch{
-            return "You've just made a misstake ;3"
+            return response.status(400).send({error: ErrorType.INVALID_PAYLOAD});
         }
 
-        response.redirect('/profile')
+        const profile = await Profile.findBy("email", auth.user!.email);
+        return {
+            id: profile!.id,
+            email: profile!.email,
+            createdAt: profile!.createdAt,
+            updatedAt: profile!.updatedAt
+        }
         
     };
     public async logout({ auth, response }: HttpContextContract) {
-        await auth.use('web').logout()
-        response.redirect('/profile/login')
+        try{
+            await auth.use('web').authenticate()
+        }
+        catch{
+            return response.status(401).send({error: ErrorType.UNAUTHORIZED});
+        }
+        await auth.use('web').logout();
+        
+        return response.status(200).send({ok: true});
     };
-    public async getProducts({ auth }: HttpContextContract) {
-        await auth.use('web').authenticate()
+    public async getProducts({ auth, response }: HttpContextContract) {
+        try{
+            await auth.use('web').authenticate()
+        }
+        catch{
+            return response.status(401).send({error: ErrorType.UNAUTHORIZED});
+        }
         const profile = await Profile.findBy("email", auth.user!.email);
         const profileProducts = await ProfileProduct.query()
             .where('profileId', profile!.id)
@@ -32,8 +50,13 @@ export default class AuthorizationController {
             count: profileProduct.productCount
         }));
     };
-    public async getProfile({auth}: HttpContextContract) {
-        await auth.use('web').authenticate()
+    public async getProfile({ auth, response }: HttpContextContract) {
+        try{
+            await auth.use('web').authenticate()
+        }
+        catch{
+            return response.status(401).send({error: ErrorType.UNAUTHORIZED});
+        }
 
         return await Profile.findBy("email", auth.user!.email);
     }
